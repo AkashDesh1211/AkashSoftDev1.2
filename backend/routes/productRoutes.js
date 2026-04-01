a8479ddaae7f1e1ebd8415a9c6a4ca0409feab08
Akash <akashdeshpande1211@gmail.com>
2026-04-01 04:01:08 +0000

    continue working on prod route codes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -9,3 +9,13 @@ const {
   deleteProduct,
 } = require("../controllers/productController");
 
+// adjust this to match your starter auth middleware filename
+const { protect } = require("../middleware/authMiddleware");
+
+router.get("/", protect, getProducts);
+router.get("/:id", protect, getSingleProduct);
+router.post("/", protect, createProduct);
+router.put("/:id", protect, updateProduct);
+router.delete("/:id", protect, deleteProduct);
+
+module.exports = router;
