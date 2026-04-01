b968397bec23b011749587a1e5892a8c05ca5dc2
Akash <akashdeshpande1211@gmail.com>
2026-04-01 04:41:37 +0000

    updating axios config file

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,9 +1,27 @@
 import axios from 'axios';
 
+// Automatically detect environment
+const baseURL =
+  window.location.hostname === 'localhost'
+    ? 'http://localhost:5001' // local dev
+    : `http://${window.location.hostname}:5001`; // EC2 auto
+
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5001', // local
-  //baseURL: 'http://3.26.96.188:5001', // live
-  headers: { 'Content-Type': 'application/json' },
+  baseURL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 export default axiosInstance;
+
+
+//import axios from 'axios';
+
+//const axiosInstance = axios.create({
+//  baseURL: 'http://localhost:5001', // local
+  //baseURL: 'http://3.26.96.188:5001', // live
+ // headers: { 'Content-Type': 'application/json' },
+//});
+
+//export default axiosInstance;
